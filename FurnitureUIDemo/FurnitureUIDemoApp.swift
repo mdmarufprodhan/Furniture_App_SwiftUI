59119f69ab6e7b90d296cbba2c51d09e386ee0c6
mdmarufprodhan <marufprodhan463@gmail.com>
2022-09-07 18:18:46 +0600

    Initial Commit

---
+++ FurnitureUIDemo/FurnitureUIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FurnitureUIDemoApp.swift
+//  FurnitureUIDemo
+//
+//  Created by Md Maruf Prodhan on 7/9/22.
+//
+
+import SwiftUI
+
+@main
+struct FurnitureUIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
